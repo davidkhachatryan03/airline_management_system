185405dc26c144c0a52902693182d10d9b3f06fb
David Khachatryan <david.v.khachatryan03@gmail.com>
2026-06-27 12:08:29 -0300

    test: complete create booking use case api testing

---
+++ tests/tests_api/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+
+@pytest.fixture
+def booking_request() -> dict:
+    return {
+        "flights_id": ["019ef4a6-dc54-7d5f-b3ba-df1acea2d9c7", "019ef4a7-2505-77c0-ab0a-dd9185248a1a"],
+        "passengers": [
+            {
+                "full_name": "David Khachatryan",
+                "birth_date": "2000-01-01",
+                "email": "email@example.com",
+                "phone_number": "5491123456789",
+                "national_identity_number": "40123456",
+                "valid_from": "2010-01-01",
+                "valid_until": "2020-01-01",
+                "issue_country": "ARG"
+            }
+        ]
+    }
