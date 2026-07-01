288f3b7dd4fc727c3635a5f31081265e36628498
David Khachatryan <david.v.khachatryan03@gmail.com>
2026-07-01 10:46:28 -0300

    test: develop testing to passenger validator

---
+++ tests/tests_validators/test_passenger_validator.py
@@ -0,0 +1,19 @@
+from uuid import UUID
+
+import pytest
+
+from src.common.exceptions import BlacklistedPassenger, InexistentPassenger
+from src.core.validators import PassengerValidator
+from src.entities import Passenger
+
+def test_passenger_validator_blacklisted_passenger(passenger_validator: PassengerValidator, passengers_requested: list[Passenger]) -> None:
+    with pytest.raises(BlacklistedPassenger):
+        passenger_validator.check_blacklisted(passengers_requested)
+
+def test_passenger_validator_inexistent_passenger_passengers_retrieved(passenger_validator: PassengerValidator, passengers_id_requested: list[UUID], passengers_retrieved: list[Passenger]) -> None:
+    with pytest.raises(InexistentPassenger):
+        passenger_validator.check_existence(passengers_id_requested, passengers_retrieved)
+
+def test_passenger_validator_inexistent_passenger_no_passengers_retrieved(passenger_validator: PassengerValidator, passengers_id_requested: list[UUID]) -> None:
+    with pytest.raises(InexistentPassenger):
+        passenger_validator.check_existence(passengers_id_requested, [])
