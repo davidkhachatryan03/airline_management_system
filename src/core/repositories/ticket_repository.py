adc0d2573d579c3d9c17a9398a075e4b5dabf1a8
David Khachatryan <david.v.khachatryan03@gmail.com>
2026-07-02 10:48:16 -0300

    feat: add retrieve method with limit

--- src/core/repositories/ticket_repository.py
+++ src/core/repositories/ticket_repository.py
@@ -5,6 +5,16 @@ class TicketRepository:
 
     def __init__(self, db_manager: DBManager) -> None:
         self.db_manager = db_manager
+    
+    def retrieve_tickets(self, limit: int = 5) -> list[Ticket]:
+        query = "SELECT * FROM passengers ORDER BY id DESC LIMIT %s"
+
+        results: list[tuple] = self.db_manager.retrieve(query, (limit,))
+        
+        if results:
+            return [Ticket(*result) for result in results]
+        
+        return []
 
     def insert_tickets(self, tickets: list[Ticket]) -> None:
         self.db_manager.insert_rows("tickets", tickets)
