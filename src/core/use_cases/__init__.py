70b5df9690deb36a2bda5057edb1346b00ac7ca3
David Khachatryan <david.v.khachatryan03@gmail.com>
2026-07-03 10:20:38 -0300

    reorder use cases into internal (operations related) and external (client actions related)

--- src/core/use_cases/__init__.py
+++ src/core/use_cases/__init__.py
@@ -1,2 +1,2 @@
-from src.core.use_cases.create_booking import CreateBooking, PassengerProcessor
-from src.core.use_cases.register_document import RegisterDocument
+from src.core.use_cases.internal.create_booking import CreateBooking, PassengerProcessor
+from src.core.use_cases.internal.register_document import RegisterDocument
