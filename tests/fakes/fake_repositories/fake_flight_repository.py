59cb21803f3cc421be69ba9ceaf666adae803f16
David Khachatryan <david.v.khachatryan03@gmail.com>
2026-07-17 18:27:32 -0300

    test: finish use case testing

--- tests/fakes/fake_repositories/fake_flight_repository.py
+++ tests/fakes/fake_repositories/fake_flight_repository.py
@@ -1,6 +1,7 @@
 from collections.abc import KeysView
 from uuid import UUID
 
+from src.common.types import FlightIdentityKey
 from src.entities import Flight
 
 class FakeFlightRepository:
@@ -14,6 +15,17 @@ class FakeFlightRepository:
 
     def retrieve_flights_by_id(self, flights_id: list[UUID]) -> list[Flight]:
         return list(self.flights.keys())
+    
+    def retrieve_flights_by_identity_key(self, flights: list[FlightIdentityKey]) -> list[Flight]:
+        flights_retrieved: list[Flight] = []
+        flights_stored: list[Flight] = list(self.flights.keys())
+        dict_flights_stored_identity_keys: dict[FlightIdentityKey, Flight] = {flight.identity_key: flight for flight in flights_stored}
+
+        for flight in flights:
+            if flight in dict_flights_stored_identity_keys:
+                flights_retrieved.append(dict_flights_stored_identity_keys[flight])
+        
+        return flights_retrieved
     
     def retrieve_seats_available_per_flight(self, flights: list[Flight]) -> dict[UUID, int]:
         flights_stored: KeysView[Flight] = self.flights.keys()
--- tests/fakes/fake_repositories/fake_route_repository.py
+++ tests/fakes/fake_repositories/fake_route_repository.py
@@ -1,3 +1,4 @@
+from src.common.types import DistanceKm, RouteId
 from src.entities import Route
 
 class FakeRouteRepository:
@@ -5,5 +6,29 @@ class FakeRouteRepository:
     def __init__(self) -> None:
         self.routes: list[Route] = []
 
-    def insert_route(self, route: Route) -> None:
-        self.routes.append(route)
+    def insert_routes(self, routes: list[Route]) -> None:
+        self.routes.extend(routes)
+
+    def retrieve_routes_by_id(self, routes: list[RouteId]) -> list[Route]:
+        routes_retrieved: list[Route] = []
+        dict_routes_stored_ids: dict[RouteId, Route] = {route.id: route for route in self.routes}
+
+        for route in routes:
+            if route in dict_routes_stored_ids:
+                routes_retrieved.append(dict_routes_stored_ids[route])
+        
+        return routes_retrieved
+
+    def retrieve_distance_km_by_id(self, route_id: RouteId) -> list[DistanceKm]:
+        for route in self.routes:
+            if route.id == route_id:
+                return [route.distance_km]
+        
+        return []
+    
+    def retrieve_duration_min_by_id(self, route_id: RouteId) -> list[DistanceKm]:
+        for route in self.routes:
+            if route.id == route_id:
+                return [route.duration_min]
+        
+        return []
